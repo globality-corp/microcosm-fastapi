e0583ecaf61250191a275cccde6f724b5ceb0407
Pierce Freeman <piercefreeman@gmail.com>
2021-02-16 14:27:29 -0800

    add pubsub handlers

---
+++ microcosm-fastapi/microcosm_fastapi/pubsub/handlers/uri_handler.py
@@ -0,0 +1,58 @@
+from microcosm_pubsub.handlers.uri_handler import URIHandler
+from abc import ABCMeta
+
+
+class URIHandlerAsync(URIHandler, metaclass=ABCMeta):
+    async def __call__(self, message):
+        uri = message["uri"]
+        self.on_call(message, uri)
+
+        skip_reason = self.get_reason_to_skip(message, uri)
+        if skip_reason is not None:
+            self.on_skip(message, uri, skip_reason)
+            return False
+
+        resource = self.convert_resource(
+            await self.get_resource(message, uri),
+        )
+
+        if await self.handle(message, uri, resource):
+            self.on_handle(message, uri, resource)
+            return True
+        else:
+            self.on_ignore(message, uri, resource)
+            return False
+
+    async def get_resource(self, message, uri):
+        """
+        Mock-friendly URI getter.
+        Passes message context.
+        """
+        if self.resource_cache and self.resource_cache_whitelist_callable(
+            media_type=message.get("mediaType"),
+            uri=uri
+        ):
+            response = self.resource_cache.get(uri)
+            if response:
+                return response
+
+        headers = self.get_headers(message)
+        # TODO: Add in async support here
+        response = get(uri, headers=headers)
+        if response.status_code == codes.not_found and self.nack_if_not_found:
+            raise Nack(self.resource_nack_timeout)
+        response.raise_for_status()
+        response_json = response.json()
+
+        self.validate_changed_field(message, response_json)
+
+        if self.resource_cache and self.resource_cache_whitelist_callable(
+            media_type=message.get("mediaType"),
+            uri=uri,
+        ):
+            self.resource_cache.set(uri, response_json, ttl=self.resource_cache_ttl)
+
+        return response_json
+
+    async def handle(self):
+        return True
