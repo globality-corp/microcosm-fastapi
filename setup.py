da67b3898defecc301e3762e6bfb94bd5ac68259
Pierce Freeman <piercefreeman@gmail.com>
2021-08-20 08:50:07 -0700

    Add globality build support (#12)
    * add globality build support
    
    * Add libpq-dev
    
    * Commenting out linting and typehinting for now - will be fixed as part of GLOB-56588
    
    Co-authored-by: John Peterson <john.peterson@globality.com>

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "microcosm-pubsub",
         "microcosm-postgres[encryption]>=2.0.0",
         "asyncpg",
+        "psycopg2-binary>=2.7.5",
     ],
     setup_requires=[
         "nose>=1.3.7",
