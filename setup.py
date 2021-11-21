d789b880149604704a72c2f0dbe23bfa41af8c18
John Peterson <john.peterson@globality.com>
2021-11-21 14:52:36 +0000

    Adding dependencies

--- setup.py
+++ setup.py
@@ -66,6 +66,8 @@ setup(
     tests_require=[
         "coverage>=3.7.1",
         "PyHamcrest>=1.9.0",
+        "httpx",
+        "pytest",
     ],
     extras_require={
         "metrics": "microcosm-metrics>=2.5.1",
