1dfcda5cccdb28c533daa2a286d3833866c743d2
John Peterson <john.peterson@globality.com>
2021-12-14 21:32:11 +0000

    Removing pip list cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,6 @@ elif [ "$1" = "lint" ]; then
    # Install standard linting dependencies; YMMV
    pip --quiet install \
        .[lint] flake8 flake8-print flake8-logging-format flake8-isort
-   pip list
    flake8 ${NAME}
 elif [ "$1" = "typehinting" ]; then
    # Install standard type-linting dependencies
